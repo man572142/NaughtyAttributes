2aa34c5e21bd41cd1c9cdf9ddd313d27d71e3a48
man572142 <man572142@gmail.com>
2025-06-09 10:44:28 +0800

    Moved DisplayOptions to an individual file

--- Assets/NaughtyAttributes/Scripts/Core/DrawerAttributes_SpecialCase/ButtonAttribute.cs
+++ Assets/NaughtyAttributes/Scripts/Core/DrawerAttributes_SpecialCase/ButtonAttribute.cs
@@ -18,33 +18,6 @@ namespace NaughtyAttributes
         Playmode
     }
 
-    [Flags]
-    public enum DisplayOptions
-    {
-        None = 0,
-
-        // Presets
-        Default = AtBottom | SingleLineHeight | FullWidth,
-        MiniBottom = AtBottom | SingleLineHeight | HalfWidth,
-        MiniTop = OnTop | SingleLineHeight | HalfWidth,
-        FatBottom = AtBottom | DoubleLineHeight | HalfWidth,
-        Big = AtBottom | TripleLineHeight | FullWidth,
-
-        // Position
-        OnTop = 1 << 0,
-        AtBottom = 1 << 1,
-        AlongSide = 1 << 2,
-
-        // Height
-        SingleLineHeight = 1 << 3,
-        DoubleLineHeight = 1 << 4,
-        TripleLineHeight = 1 << 5,
-
-        // Width
-        FullWidth = 1 << 6,
-        HalfWidth = 1 << 7,
-    }
-
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Field, AllowMultiple = true, Inherited = true)]
     public class ButtonAttribute : SpecialCaseDrawerAttribute
     {
---
+++ Assets/NaughtyAttributes/Scripts/Core/DrawerAttributes_SpecialCase/DisplayOptions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NaughtyAttributes
+{
+    [Flags]
+    public enum DisplayOptions
+    {
+        None = 0,
+
+        // Presets
+        Default = AtBottom | SingleLineHeight | FullWidth,
+        MiniBottom = AtBottom | SingleLineHeight | HalfWidth,
+        MiniTop = OnTop | SingleLineHeight | HalfWidth,
+        FatBottom = AtBottom | DoubleLineHeight | HalfWidth,
+        Big = AtBottom | TripleLineHeight | FullWidth,
+
+        // Position
+        OnTop = 1 << 0,
+        AtBottom = 1 << 1,
+        AlongSide = 1 << 2,
+
+        // Height
+        SingleLineHeight = 1 << 3,
+        DoubleLineHeight = 1 << 4,
+        TripleLineHeight = 1 << 5,
+
+        // Width
+        FullWidth = 1 << 6,
+        HalfWidth = 1 << 7,
+    }
+}
